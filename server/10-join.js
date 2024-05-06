460336c348a388d06eeefb0dfe4397bb4963ea86
tefosc <asolism17_1@unc.edu.pe>
2024-05-06 11:15:18 -0500

    Add join method and split

---
+++ server/10-join.js
@@ -0,0 +1,10 @@
+const elements = ["Fire", "Air", "Water"];
+
+const rptaJoin = elements.join("--");
+console.log(rptaJoin);
+
+const title = "Curso de manipulación de arrays";
+console.log(title.split(" "));
+
+const titleFinal = title.split(" ").join("-").toLocaleUpperCase();
+console.log(titleFinal);
